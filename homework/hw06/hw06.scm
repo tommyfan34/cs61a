cb279eeb137ec31d44c755ce658053a2516a76fc
Xiao Fan <tommyfanzju@gmail.com>
2021-03-12 22:40:48 +0800

    finished hw06

---
+++ homework/hw06/hw06.scm
@@ -0,0 +1,29 @@
+(define (cddr s)
+  (cdr (cdr s)))
+
+(define (cadr s)
+  (car (cdr s))
+)
+
+(define (caddr s)
+  (car (cddr s))
+)
+
+
+(define (sign num)
+  (cond ((< num 0) -1)
+        ((= num 0) 0)
+        ((> num 0) 1))
+)
+
+
+(define (square x) (* x x))
+
+(define (pow x y)
+  (cond ((= x 1) 1)
+        ((= y 1) x)
+        ((= y 2) (square x))
+        ((even? y) (square (pow x (/ y 2))))
+        ((odd? y) (* x (square (pow x (/ (- y 1) 2))))))
+)
+
